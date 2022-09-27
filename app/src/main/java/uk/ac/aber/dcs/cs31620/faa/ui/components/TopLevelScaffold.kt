528bc43c68d03132648f88c2242faf361337ddf0
cwl <cwl@aber.ac.uk>
2022-09-27 16:56:31 +0100

    Updates to use Material 3 Scaffold, replacing Material 2 version.

--- app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/components/TopLevelScaffold.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/components/TopLevelScaffold.kt
@@ -1,12 +1,11 @@
 package uk.ac.aber.dcs.cs31620.faa.ui.components
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavHostController
-import kotlinx.coroutines.launch
+
 /**
  * Creates the page scaffold to contain top app bar,
  * bottom navigation button and of course the page content.
@@ -15,14 +14,13 @@ import kotlinx.coroutines.launch
  * By default an empty lambda.
  * @author Chris Loftus
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopLevelScaffold(
     navController: NavHostController,
     pageContent: @Composable (innerPadding: PaddingValues) -> Unit = {}
 ) {
-    val scaffoldState = rememberScaffoldState()
     Scaffold(
-        scaffoldState = scaffoldState,
         topBar = {
             MainPageTopAppBar()
         },
